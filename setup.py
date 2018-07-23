61d1780784a06e2a5317236b6643b0316cce2bb2
tslight <tobyslight@gmail.com>
2018-07-23 08:55:40 +0100

    lazygitpush

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pdu",
-    version="0.0.2",
+    version="0.0.3",
     author="Toby Slight",
     author_email="tobyslight@gmail.com",
     description="Python Disk Usage",
